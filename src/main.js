f797a93ce93f744f711649b2e4482979c9234c21
Karel Frederix <kfrederix@selligent.com>
2023-09-28 21:29:18 +0200

    format

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 const monacoEl = document.getElementById('monaco-editor');
 
 monaco.editor.create(monacoEl, {
-	value: `const foo = () => 0;`,
-	language: 'javascript',
-	automaticLayout: true,
+  value: `const foo = () => 0;`,
+  language: 'javascript',
+  automaticLayout: true,
 });
